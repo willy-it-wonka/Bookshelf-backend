28dde6451e53a86da4d85bd86d5d47bcdf44de42
Willy <willy.it.wonka@outlook.com>
2024-03-16 21:30:48 +0100

    Replace getters and setters with Lombok annotations

--- src/main/java/com/mybooks/bookshelfSB/book/Book.java
+++ src/main/java/com/mybooks/bookshelfSB/book/Book.java
@@ -1,12 +1,16 @@
 package com.mybooks.bookshelfSB.book;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "books")
 @EntityListeners(AuditingEntityListener.class)
@@ -37,64 +41,11 @@ public class Book {
     @Column(name = "link_to_cover")
     private String linkToCover;
 
-    public Book() {
-    }
+    public Book() {}
 
     public Book(String title, String author, BookStatus status) {
         this.title = title;
         this.author = author;
         this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public LocalDateTime getCreatedDate() {
-        return createdDate;
-    }
-
-    public LocalDateTime getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(LocalDateTime lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public BookStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(BookStatus status) {
-        this.status = status;
-    }
-
-    public String getLinkToCover() {
-        return linkToCover;
-    }
-
-    public void setLinkToCover(String linkToCover) {
-        this.linkToCover = linkToCover;
     }
 }
