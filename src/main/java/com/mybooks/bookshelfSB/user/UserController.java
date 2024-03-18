b251edfe218c868eea765eb6bd0c84bc25ddf4bd
Willy <willy.it.wonka@outlook.com>
2024-03-18 22:01:44 +0100

    Add user registration function

--- src/main/java/com/mybooks/bookshelfSB/user/UserController.java
+++ src/main/java/com/mybooks/bookshelfSB/user/UserController.java
@@ -1,9 +1,7 @@
 package com.mybooks.bookshelfSB.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -17,5 +15,9 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/register")
+    public String createUser(@RequestBody UserDto userDto) {
+        return userService.createUser(userDto);
+    }
 
 }
--- src/main/java/com/mybooks/bookshelfSB/user/UserService.java
+++ src/main/java/com/mybooks/bookshelfSB/user/UserService.java
@@ -1,16 +1,28 @@
 package com.mybooks.bookshelfSB.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public String createUser(UserDto userDto) {
+        User user = new User(
+                userDto.getNick(),
+                userDto.getEmail(),
+                this.passwordEncoder.encode(userDto.getPassword()),
+                UserRole.USER);
+        userRepository.save(user);
+        return user.getNick();
+    }
 }
