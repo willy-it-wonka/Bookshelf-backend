343db7e2bcd3f6c22faea2c4825d938a262c39b4
Willy <willy.it.wonka@outlook.com>
2024-03-18 21:33:41 +0100

    Add UserDto
    Data Transfer Object is used to transfer data between different layers of an application.

---
+++ src/main/java/com/mybooks/bookshelfSB/user/UserDto.java
@@ -0,0 +1,17 @@
+package com.mybooks.bookshelfSB.user;
+
+import lombok.Getter;
+
+// Data Transfer Object is used to transfer data between different layers of an application.
+@Getter
+public class UserDto {
+    private final String nick;
+    private final String email;
+    private final String password;
+
+    public UserDto(String nick, String email, String password) {
+        this.nick = nick;
+        this.email = email;
+        this.password = password;
+    }
+}
