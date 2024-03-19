9e49f4d5a582f01d27a2de4db588cd2b780bc021
Willy <willy.it.wonka@outlook.com>
2024-03-19 21:39:17 +0100

    Add EmailService

---
+++ src/main/java/com/mybooks/bookshelfSB/user/email/EmailService.java
@@ -0,0 +1,17 @@
+package com.mybooks.bookshelfSB.user.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+}
