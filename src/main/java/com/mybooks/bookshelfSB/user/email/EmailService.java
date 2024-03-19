8916a5d793b23ec6c362a3c2e60da28c5a694ce7
Willy <willy.it.wonka@outlook.com>
2024-03-19 22:21:40 +0100

    Crete and configure MimeMessage for confirmation mail

--- src/main/java/com/mybooks/bookshelfSB/user/email/EmailService.java
+++ src/main/java/com/mybooks/bookshelfSB/user/email/EmailService.java
@@ -1,7 +1,11 @@
 package com.mybooks.bookshelfSB.user.email;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,4 +18,18 @@ public class EmailService {
         this.javaMailSender = javaMailSender;
     }
 
+    // Send confirmation mail.
+    @Async // will make it execute in a separate thread
+    public void send(String addressee, String email) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
+            mimeMessageHelper.setText(email, true);
+            mimeMessageHelper.setTo(addressee);
+            mimeMessageHelper.setSubject("Confirm your email");
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new IllegalStateException("Failed to send email.");
+        }
+    }
 }
