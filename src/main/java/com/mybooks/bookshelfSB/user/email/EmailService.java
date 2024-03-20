b1a7697eee0040f2c1d74093ae1dbf0a7dbbe5aa
Willy <willy.it.wonka@outlook.com>
2024-03-20 21:39:18 +0100

    Change MimeMessage
    setFrom is necessarily

--- src/main/java/com/mybooks/bookshelfSB/user/email/EmailService.java
+++ src/main/java/com/mybooks/bookshelfSB/user/email/EmailService.java
@@ -4,32 +4,39 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
+    private final JavaMailSenderImpl fromProperties;
 
     @Autowired
-    public EmailService(JavaMailSender javaMailSender) {
+    public EmailService(JavaMailSender javaMailSender, JavaMailSenderImpl fromProperties) {
         this.javaMailSender = javaMailSender;
+        this.fromProperties = fromProperties;
     }
 
     // Send confirmation mail.
     @Async // will make it execute in a separate thread
-    public void send(String addressee, String email) {
+    public void send(String addressee, String message) {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
             MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, "utf-8");
-            mimeMessageHelper.setText(email, true);
+            mimeMessageHelper.setText(message, true);
             mimeMessageHelper.setTo(addressee);
             mimeMessageHelper.setSubject("Confirm your email");
+            mimeMessageHelper.setFrom(Objects.requireNonNull(fromProperties.getUsername()));
             javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
             throw new IllegalStateException("Failed to send email.");
         }
     }
+
 }
