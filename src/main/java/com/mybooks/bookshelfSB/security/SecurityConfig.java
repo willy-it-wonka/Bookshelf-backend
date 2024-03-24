a9abfb8e8233d322fdf483c5072ffd7ce3d499b9
Willy <willy.it.wonka@outlook.com>
2024-03-24 20:50:05 +0100

    Configure security

--- src/main/java/com/mybooks/bookshelfSB/security/SecurityConfig.java
+++ src/main/java/com/mybooks/bookshelfSB/security/SecurityConfig.java
@@ -2,11 +2,27 @@ package com.mybooks.bookshelfSB.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/register/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
