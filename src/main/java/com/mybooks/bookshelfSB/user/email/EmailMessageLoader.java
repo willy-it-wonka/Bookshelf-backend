9eef3dac573f9dab3c442541737751bb0314df3f
Willy <willy.it.wonka@outlook.com>
2024-04-19 22:43:40 +0200

    Refactor EmailMessageLoader

--- src/main/java/com/mybooks/bookshelfSB/user/email/EmailMessageLoader.java
+++ src/main/java/com/mybooks/bookshelfSB/user/email/EmailMessageLoader.java
@@ -1,9 +1,9 @@
 package com.mybooks.bookshelfSB.user.email;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -17,8 +17,7 @@ public class EmailMessageLoader {
             Resource resource = new ClassPathResource(location);
             // First get content of file, then convert to String using IOUtils from Apache Commons IO.
             return IOUtils.toString(resource.getInputStream(), StandardCharsets.UTF_8);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new IllegalStateException("Failed to load email message template.");
         }
     }
