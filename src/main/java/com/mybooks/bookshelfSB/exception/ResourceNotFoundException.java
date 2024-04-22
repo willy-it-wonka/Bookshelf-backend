b8d1f6803d29885a12075fb0c36496b6a833afd1
Willy <willy.it.wonka@outlook.com>
2024-04-22 20:29:46 +0200

    Refactor exceptions

--- src/main/java/com/mybooks/bookshelfSB/exception/ResourceNotFoundException.java
+++ src/main/java/com/mybooks/bookshelfSB/exception/ResourceNotFoundException.java
@@ -3,14 +3,18 @@ package com.mybooks.bookshelfSB.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 //If a ResourceNotFoundException is thrown, the HTTP server will return a message with the status code 404 (Not Found).
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     //Unique IDentifier used to identify the version of class during serialization and deserialization.
+    @Serial
     private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(Long id) {
-        super("A book with this ID doesn't exist: " + id);
+        super(String.format("Book with ID: %s doesn't exist.", id));
     }
+
 }
