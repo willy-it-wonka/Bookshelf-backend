b50b086a445729196eed60f08150e267e631c1b3
Willy <willy.it.wonka@outlook.com>
2024-04-26 19:20:33 +0200

    Add GlobalExceptionHandler

---
+++ src/main/java/com/mybooks/bookshelfSB/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.mybooks.bookshelfSB.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EmailIssueException.class)
+    public ResponseEntity<String> handleEmailIssueException(EmailIssueException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UnauthorizedAccessException.class)
+    public ResponseEntity<String> handleUnauthorizedAccessException(UnauthorizedAccessException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    }
+
+}
