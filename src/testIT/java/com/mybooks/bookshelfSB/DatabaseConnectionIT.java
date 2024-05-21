cfbc0a05ac6af7c8f5f30774090eccf05841d5c6
Willy <willy.it.wonka@outlook.com>
2024-05-21 21:00:29 +0200

    Test database connection

---
+++ src/testIT/java/com/mybooks/bookshelfSB/DatabaseConnectionIT.java
@@ -0,0 +1,44 @@
+package com.mybooks.bookshelfSB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Testcontainers
+public class DatabaseConnectionIT {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    // 1. Create and configure MySQL container that will automatically start before the tests and stop after.
+    @Container
+    public static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testDB")
+            .withUsername("test")
+            .withPassword("test");
+
+    // 2. Inject the database connection properties into the Spring Application context.
+    @DynamicPropertySource
+    static void databaseProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
+
+    @Test
+    void testDatabaseConnection() {
+        jdbcTemplate.update("DELETE FROM users");
+        Integer count = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
+        assertEquals(0, count);
+    }
+
+}
