0ecbf6068ab94ed21ae49c24ef37af7238e22101
Willy <willy.it.wonka@outlook.com>
2024-06-08 23:13:51 +0200

    Add NoteRepository

---
+++ src/main/java/com/mybooks/bookshelfSB/book/note/NoteRepository.java
@@ -0,0 +1,13 @@
+package com.mybooks.bookshelfSB.book.note;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    Optional<Note> findByBookId(Long bookId);
+
+}
