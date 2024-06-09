76f047977b5d476a5d607662b948b91adaba1e5d
Willy <willy.it.wonka@outlook.com>
2024-06-09 22:50:29 +0200

    Add NoteService

---
+++ src/main/java/com/mybooks/bookshelfSB/book/note/NoteService.java
@@ -0,0 +1,34 @@
+package com.mybooks.bookshelfSB.book.note;
+
+import com.mybooks.bookshelfSB.exception.NoteNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    Note getNoteByBookId(Long bookId) {
+        return noteRepository.findByBookId(bookId).orElseThrow(() -> new NoteNotFoundException(bookId));
+    }
+
+    Note createNote(Note note) {
+        return noteRepository.save(note);
+    }
+
+    Note updateNote(Long bookId, Note note) {
+        Note noteToUpdate = getNoteByBookId(bookId);
+        noteToUpdate.setContent(note.getContent());
+        return noteRepository.save(noteToUpdate);
+    }
+
+    void deleteNoteByBookId(Long bookId) {
+        Note note = getNoteByBookId(bookId);
+        noteRepository.delete(note);
+    }
+
+}
