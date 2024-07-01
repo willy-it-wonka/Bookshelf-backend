73b7f30bd750f62af43f10b985a8979b83c2243b
Willy <willy.it.wonka@outlook.com>
2024-07-01 22:44:39 +0200

    Implement NoteDto

--- src/main/java/com/mybooks/bookshelfSB/book/note/NoteController.java
+++ src/main/java/com/mybooks/bookshelfSB/book/note/NoteController.java
@@ -1,6 +1,7 @@
 package com.mybooks.bookshelfSB.book.note;
 
 import com.mybooks.bookshelfSB.book.note.payload.CreateNoteRequest;
+import com.mybooks.bookshelfSB.book.note.payload.NoteDto;
 import com.mybooks.bookshelfSB.book.note.payload.UpdateNoteRequest;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,23 +16,33 @@ public class NoteController {
     }
 
     @GetMapping("/notes/{bookId}")
-    public Note getNoteByBookId(@PathVariable Long bookId) {
-        return noteService.getNoteByBookId(bookId);
+    public NoteDto getNoteByBookId(@PathVariable Long bookId) {
+        Note note = noteService.getNoteByBookId(bookId);
+        return convertToDto(note);
     }
 
     @PostMapping("/notes")
-    public Note createNote(@RequestBody CreateNoteRequest request) {
-        return noteService.createNote(request);
+    public NoteDto createNote(@RequestBody CreateNoteRequest request) {
+        Note note = noteService.createNote(request);
+        return convertToDto(note);
     }
 
     @PutMapping("/notes/{bookId}")
-    public Note updateNote(@PathVariable Long bookId, @RequestBody UpdateNoteRequest request) {
-        return noteService.updateNote(bookId, request);
+    public NoteDto updateNote(@PathVariable Long bookId, @RequestBody UpdateNoteRequest request) {
+        Note note = noteService.updateNote(bookId, request);
+        return convertToDto(note);
     }
 
     @DeleteMapping("/notes/{bookId}")
     public void deleteNoteByBookId(@PathVariable Long bookId) {
         noteService.deleteNoteByBookId(bookId);
+    }
+
+    private NoteDto convertToDto(Note note) {
+        return new NoteDto(
+                note.getId(),
+                note.getContent(),
+                note.getBook().getId());
     }
 
 }
---
+++ src/main/java/com/mybooks/bookshelfSB/book/note/payload/NoteDto.java
@@ -0,0 +1,4 @@
+package com.mybooks.bookshelfSB.book.note.payload;
+
+public record NoteDto(Long id, String content, Long bookId) {
+}
