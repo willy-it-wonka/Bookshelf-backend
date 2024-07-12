bc050e4cdda6c7426bf07d054ffc7b794e19de60
Willy <willy.it.wonka@outlook.com>
2024-07-12 22:35:20 +0200

    Refactor GlobalExceptionHandler

--- src/main/java/com/mybooks/bookshelfSB/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mybooks/bookshelfSB/exception/GlobalExceptionHandler.java
@@ -1,42 +1,56 @@
 package com.mybooks.bookshelfSB.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import static org.springframework.http.HttpStatus.*;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailIssueException.class)
-    public ResponseEntity<String> handleEmailIssueException(EmailIssueException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    public String handleEmailIssueException(EmailIssueException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler(BookNotFoundException.class)
-    public ResponseEntity<String> handleBookNotFoundException(BookNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    @ResponseStatus(NOT_FOUND)
+    @ResponseBody
+    public String handleBookNotFoundException(BookNotFoundException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler(NoteNotFoundException.class)
-    public ResponseEntity<String> handleNoteNotFoundException(NoteNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    @ResponseStatus(NOT_FOUND)
+    @ResponseBody
+    public String handleNoteNotFoundException(NoteNotFoundException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler(UnauthorizedAccessException.class)
-    public ResponseEntity<String> handleUnauthorizedAccessException(UnauthorizedAccessException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+    @ResponseStatus(FORBIDDEN)
+    @ResponseBody
+    public String handleUnauthorizedAccessException(UnauthorizedAccessException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ResponseEntity<String> handleIllegalStateException(IllegalStateException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    public String handleIllegalStateException(IllegalStateException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    @ResponseStatus(BAD_REQUEST)
+    @ResponseBody
+    public String handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        return e.getMessage();
     }
 
 }
