0a12d001cba705756bb686301e02cdc076996483
Willy <willy.it.wonka@outlook.com>
2024-07-13 15:27:16 +0200

    Refactor exception classes

--- src/main/java/com/mybooks/bookshelfSB/exception/BookNotFoundException.java
+++ src/main/java/com/mybooks/bookshelfSB/exception/BookNotFoundException.java
@@ -1,17 +1,6 @@
 package com.mybooks.bookshelfSB.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.io.Serial;
-
-//If a BookNotFoundException is thrown, the HTTP server will return a message with the status code 404 (Not Found).
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException {
-
-    //Unique IDentifier used to identify the version of class during serialization and deserialization.
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     public BookNotFoundException(Long id) {
         super(String.format("Book with ID: %s doesn't exist.", id));
--- src/main/java/com/mybooks/bookshelfSB/exception/EmailIssueException.java
+++ src/main/java/com/mybooks/bookshelfSB/exception/EmailIssueException.java
@@ -1,11 +1,6 @@
 package com.mybooks.bookshelfSB.exception;
 
-import java.io.Serial;
-
 public class EmailIssueException extends RuntimeException {
-
-    @Serial
-    private static final long serialVersionUID = 1L;
 
     public EmailIssueException(String message) {
         super(String.format("This email %s.", message));
