43aaf104b561cbc719862a452819b391fedbf3ac
Willy <willy.it.wonka@outlook.com>
2024-08-23 21:37:25 +0200

    Remove concatenation in `@Query`

--- src/main/java/com/mybooks/bookshelf/user/token/TokenRepository.java
+++ src/main/java/com/mybooks/bookshelf/user/token/TokenRepository.java
@@ -16,7 +16,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 
     @Transactional
     @Modifying
-    @Query("UPDATE Token t " + "SET t.confirmationDate = ?2 " + "WHERE t.confirmationToken = ?1")
+    @Query("UPDATE Token t SET t.confirmationDate = ?2 WHERE t.confirmationToken = ?1")
     int updateConfirmationDate(String confirmationToken, LocalDateTime confirmationDate);
 
 }
