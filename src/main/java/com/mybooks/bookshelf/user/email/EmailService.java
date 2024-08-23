d86aa30cde1763286c32d5cbf8c0d4f0c4d13e93
Willy <willy.it.wonka@outlook.com>
2024-08-23 23:38:59 +0200

    Develop exception handling in EmailService

--- src/main/java/com/mybooks/bookshelf/user/email/EmailService.java
+++ src/main/java/com/mybooks/bookshelf/user/email/EmailService.java
@@ -2,6 +2,8 @@ package com.mybooks.bookshelf.user.email;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.MailSendException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -11,11 +13,13 @@ import org.springframework.stereotype.Service;
 import java.util.Objects;
 
 @Service
+@Slf4j
 public class EmailService {
 
     private static final String ENCODING = "utf-8";
     private static final String EMAIL_SUBJECT = "Confirm your email";
-    private static final String SENDING_EMAIL_ERROR = "Failed to send email.";
+    private static final String SENDING_EMAIL_ERROR = "An error occurred when sending the confirmation email:";
+    private static final String ERROR_MESSAGE = "There was a problem while sending confirmation email. Contact the administration. You can log in and use the application.";
     private static final String TEMPLATE_MESSAGE_PATH = "templates/message.html";
     private static final String NAME_PLACEHOLDER = "{name}";
     private static final String LINK_PLACEHOLDER = "{link}";
@@ -31,7 +35,7 @@ public class EmailService {
     }
 
     // Send confirmation mail.
-    @Async // will make it execute in a separate thread
+    @Async // Will make it execute in a separate thread.
     public void send(String addressee, String message) {
         try {
             MimeMessage mimeMessage = javaMailSender.createMimeMessage();
@@ -41,8 +45,9 @@ public class EmailService {
             mimeMessageHelper.setSubject(EMAIL_SUBJECT);
             mimeMessageHelper.setFrom(Objects.requireNonNull(fromProperties.getUsername()));
             javaMailSender.send(mimeMessage);
-        } catch (MessagingException e) {
-            throw new IllegalStateException(SENDING_EMAIL_ERROR);
+        } catch (MessagingException | MailSendException e) {
+            log.error(SENDING_EMAIL_ERROR, e);
+            throw new IllegalStateException(ERROR_MESSAGE);
         }
     }
 
