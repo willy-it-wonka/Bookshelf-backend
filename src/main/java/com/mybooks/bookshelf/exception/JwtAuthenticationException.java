c6b37dd1492f9fa48e4db54ad4ace5666e874b1f
Willy <willy.it.wonka@outlook.com>
2024-09-04 23:59:19 +0200

    Create JwtAuthenticationException
    JWT is used for authorization, but this exception will be used in cases such as verifying the integrity, format, and expiry of the token, which is part of the authentication process, which is why this is the name of this exception.

---
+++ src/main/java/com/mybooks/bookshelf/exception/JwtAuthenticationException.java
@@ -0,0 +1,13 @@
+package com.mybooks.bookshelf.exception;
+
+public class JwtAuthenticationException extends RuntimeException {
+
+    public JwtAuthenticationException(String message) {
+        super(message);
+    }
+
+    public JwtAuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
