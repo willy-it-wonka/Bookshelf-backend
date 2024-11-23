c565786efcbb9cc59d5cc8221a4ad196f2fd9185
Willy <willy.it.wonka@outlook.com>
2024-11-23 22:11:28 +0100

    Create InitiateResetPasswordRequest

---
+++ src/main/java/com/mybooks/bookshelf/user/payload/InitiateResetPasswordRequest.java
@@ -0,0 +1,9 @@
+package com.mybooks.bookshelf.user.payload;
+
+import jakarta.validation.constraints.Email;
+
+public record InitiateResetPasswordRequest(
+        @Email(message = "Invalid email format.",
+                regexp = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}")
+        String email) {
+}
