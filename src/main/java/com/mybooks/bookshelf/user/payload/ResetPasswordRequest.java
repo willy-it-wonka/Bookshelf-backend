c6648774c477f2c457484d1e3cabe76e0d74629c
Willy <willy.it.wonka@outlook.com>
2024-11-23 22:13:14 +0100

    Create ResetPasswordRequest

---
+++ src/main/java/com/mybooks/bookshelf/user/payload/ResetPasswordRequest.java
@@ -0,0 +1,11 @@
+package com.mybooks.bookshelf.user.payload;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ResetPasswordRequest(
+        String token,
+        @NotBlank(message = "Password cannot be empty.")
+        @Size(min = 6, message = "Password must be at least 6 characters long.")
+        String newPassword) {
+}
