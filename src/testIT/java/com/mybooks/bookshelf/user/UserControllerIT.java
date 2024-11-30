0d6212433512d0c9db4931d81f02e7ac3e848c9c
Willy <willy.it.wonka@outlook.com>
2024-11-30 19:20:42 +0100

    Extend UserControllerIT

--- src/testIT/java/com/mybooks/bookshelf/user/UserControllerIT.java
+++ src/testIT/java/com/mybooks/bookshelf/user/UserControllerIT.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mybooks.bookshelf.email.token.TokenService;
 import com.mybooks.bookshelf.exception.EmailException;
 import com.mybooks.bookshelf.exception.IncorrectPasswordException;
+import com.mybooks.bookshelf.exception.TokenException;
 import com.mybooks.bookshelf.exception.UserNotFoundException;
 import com.mybooks.bookshelf.user.payload.*;
 import org.junit.jupiter.api.Test;
@@ -28,13 +29,22 @@ class UserControllerIT {
     private static final String USER_ID = "1";
     private static final String NONEXISTENT_USER_ID = "999";
     private static final String CORRECT_PASSWORD = "correctPassword";
-    private static final String WRONG_PASSWORD = "wrongPassword";
+    private static final String WRONG_PASSWORD = "123";
+    private static final String NEW_PASSWORD = "newPassword";
     private static final String EXISTING_EMAIL = "existing@test.com";
+    private static final String NONEXISTENT_EMAIL = "nonexistent@test.com";
+    private static final String VALID_TOKEN = "token";
+    private static final String INVALID_TOKEN = "invalid-token";
+    private static final String EXPIRED_TOKEN = "expired-token";
     private static final String NEW_NICK = "newNick";
     private static final String NEW_EMAIL = "newEmail@test.com";
     private static final String USER_NOT_FOUND_ERROR = "User not found.";
+    private static final String TOKEN_NOT_FOUND_ERROR = "Token not found.";
+    private static final String TOKEN_EXPIRED_ERROR = "Token expired.";
     private static final String EMAIL_ALREADY_TAKEN_ERROR = "This email is already associated with some account.";
     private static final String INCORRECT_PASSWORD_ERROR = "Incorrect password.";
+    private static final String INVALID_EMAIL_ERROR = "Invalid email format.";
+    private static final String SHORT_PASSWORD_ERROR = "Password must be at least 6 characters long.";
     private static final String EMAIL_SENT_MESSAGE = "A new email has been sent.";
 
     @Autowired
@@ -49,7 +59,7 @@ class UserControllerIT {
     @Test
     void whenCorrectRegisterRequest_CreateUserAndRegisterResponse() throws Exception {
         RegisterRequest request = new RegisterRequest("user", "user@test.com", "123456");
-        RegisterResponse response = new RegisterResponse("nick", "token");
+        RegisterResponse response = new RegisterResponse("user", VALID_TOKEN);
         when(userService.createUser(any(RegisterRequest.class))).thenReturn(response);
 
         mockMvc.perform(post("/api/v1/users")
@@ -57,8 +67,8 @@ class UserControllerIT {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(response)))
-                .andExpect(jsonPath("$.nick").value("nick"))
-                .andExpect(jsonPath("$.token").value("token"));
+                .andExpect(jsonPath("$.nick").value("user"))
+                .andExpect(jsonPath("$.token").value(VALID_TOKEN));
     }
 
     @Test
@@ -81,7 +91,7 @@ class UserControllerIT {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string("Invalid email format."));
+                .andExpect(content().string(INVALID_EMAIL_ERROR));
     }
 
     @Test
@@ -92,17 +102,16 @@ class UserControllerIT {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
-                .andExpect(content().string("Password must be at least 6 characters long."));
+                .andExpect(content().string(SHORT_PASSWORD_ERROR));
     }
 
     @Test
     void whenTokenIsValid_ReturnConfirmationMessage() throws Exception {
-        String validToken = "token";
         RedirectView response = new RedirectView("/confirmation-success.html");
         when(tokenService.confirmAccountActivationToken(anyString())).thenReturn(response);
 
         mockMvc.perform(get("/api/v1/users/confirmation")
-                        .param("token", validToken)
+                        .param("token", VALID_TOKEN)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(redirectedUrl("/confirmation-success.html"));
@@ -110,16 +119,14 @@ class UserControllerIT {
 
     @Test
     void whenTokenIsExpired_ReturnErrorMessage() throws Exception {
-        String expiredToken = "expired-token";
-        String errorMessage = "Token expired.";
-        RedirectView response = new RedirectView("/confirmation-error.html?error=" + errorMessage);
+        RedirectView response = new RedirectView("/confirmation-error.html?error=" + TOKEN_EXPIRED_ERROR);
         when(tokenService.confirmAccountActivationToken(anyString())).thenReturn(response);
 
         mockMvc.perform(get("/api/v1/users/confirmation")
-                        .param("token", expiredToken)
+                        .param("token", EXPIRED_TOKEN)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/confirmation-error.html?error=" + errorMessage));
+                .andExpect(redirectedUrl("/confirmation-error.html?error=" + TOKEN_EXPIRED_ERROR));
     }
 
     @Test
@@ -150,7 +157,7 @@ class UserControllerIT {
 
     @Test
     void whenTriesLoginNonExistentUser_ReturnErrorMessage() throws Exception {
-        LoginRequest request = new LoginRequest("nonexistent@test.com", CORRECT_PASSWORD);
+        LoginRequest request = new LoginRequest(NONEXISTENT_EMAIL, CORRECT_PASSWORD);
         doThrow(new UserNotFoundException()).when(userService).loginUser(request);
 
         mockMvc.perform(post("/api/v1/users/session")
@@ -292,7 +299,7 @@ class UserControllerIT {
     @Test
     void whenChangePasswordRequest_ChangePasswordAndReturnSuccessMessage() throws Exception {
         String successMessage = "Your password has been successfully changed.";
-        ChangePasswordRequest request = new ChangePasswordRequest("newPassword", CORRECT_PASSWORD);
+        ChangePasswordRequest request = new ChangePasswordRequest(NEW_PASSWORD, CORRECT_PASSWORD);
         ChangeResponse response = new ChangeResponse(successMessage);
         when(userService.changeUserPassword(eq(USER_ID), any(ChangePasswordRequest.class))).thenReturn(response);
 
@@ -306,7 +313,7 @@ class UserControllerIT {
 
     @Test
     void whenChangePasswordRequestWithIncorrectCurrentPassword_ReturnErrorMessage() throws Exception {
-        ChangePasswordRequest request = new ChangePasswordRequest("newPassword", WRONG_PASSWORD);
+        ChangePasswordRequest request = new ChangePasswordRequest(NEW_PASSWORD, WRONG_PASSWORD);
         doThrow(new IncorrectPasswordException()).when(userService).changeUserPassword(anyString(), any(ChangePasswordRequest.class));
 
         mockMvc.perform(patch("/api/v1/users/{id}/password", USER_ID)
@@ -318,7 +325,7 @@ class UserControllerIT {
 
     @Test
     void whenChangePasswordRequestForNonexistentUserId_ReturnErrorMessage() throws Exception {
-        ChangePasswordRequest request = new ChangePasswordRequest("newPassword", CORRECT_PASSWORD);
+        ChangePasswordRequest request = new ChangePasswordRequest(NEW_PASSWORD, CORRECT_PASSWORD);
         doThrow(new UserNotFoundException()).when(userService).changeUserPassword(eq(NONEXISTENT_USER_ID), any(ChangePasswordRequest.class));
 
         mockMvc.perform(patch("/api/v1/users/{id}/password", NONEXISTENT_USER_ID)
@@ -326,6 +333,77 @@ class UserControllerIT {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string(USER_NOT_FOUND_ERROR));
+    }
+
+    @Test
+    void whenInitiateForgottenPasswordResetRequest_ReturnSuccessMessage() throws Exception {
+        InitiateResetPasswordRequest request = new InitiateResetPasswordRequest("user@test.com");
+        when(userService.initiateForgottenPasswordReset(any(InitiateResetPasswordRequest.class))).thenReturn(EMAIL_SENT_MESSAGE);
+
+        mockMvc.perform(post("/api/v1/users/forgotten-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(EMAIL_SENT_MESSAGE));
+    }
+
+    @Test
+    void whenInitiateForgottenPasswordResetWithInvalidEmail_ReturnBadRequest() throws Exception {
+        InitiateResetPasswordRequest request = new InitiateResetPasswordRequest("invalid-email");
+
+        mockMvc.perform(post("/api/v1/users/forgotten-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(INVALID_EMAIL_ERROR));
+    }
+
+    @Test
+    void whenInitiateForgottenPasswordResetForNonexistentEmail_ReturnErrorMessage() throws Exception {
+        InitiateResetPasswordRequest request = new InitiateResetPasswordRequest(NONEXISTENT_EMAIL);
+        doThrow(new UserNotFoundException()).when(userService).initiateForgottenPasswordReset(any(InitiateResetPasswordRequest.class));
+
+        mockMvc.perform(post("/api/v1/users/forgotten-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(USER_NOT_FOUND_ERROR));
+    }
+
+    @Test
+    void whenResetPasswordRequest_ReturnSuccessMessage() throws Exception {
+        ResetPasswordRequest request = new ResetPasswordRequest(VALID_TOKEN, NEW_PASSWORD);
+        String successMessage = "Your password has been successfully reset.";
+        when(userService.resetForgottenPassword(any(ResetPasswordRequest.class))).thenReturn(successMessage);
+
+        mockMvc.perform(patch("/api/v1/users/password-reset")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(successMessage));
+    }
+
+    @Test
+    void whenResetPasswordWithInvalidToken_ReturnErrorMessage() throws Exception {
+        ResetPasswordRequest request = new ResetPasswordRequest(INVALID_TOKEN, NEW_PASSWORD);
+        doThrow(new TokenException(TOKEN_NOT_FOUND_ERROR)).when(userService).resetForgottenPassword(any(ResetPasswordRequest.class));
+
+        mockMvc.perform(patch("/api/v1/users/password-reset")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(TOKEN_NOT_FOUND_ERROR));
+    }
+
+    @Test
+    void whenResetPasswordWithShortPassword_ReturnBadRequest() throws Exception {
+        ResetPasswordRequest request = new ResetPasswordRequest(VALID_TOKEN, WRONG_PASSWORD);
+
+        mockMvc.perform(patch("/api/v1/users/password-reset")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(SHORT_PASSWORD_ERROR));
     }
 
 }
